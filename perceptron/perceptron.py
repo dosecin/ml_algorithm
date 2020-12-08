7bd3010b4fe4b2e369b4f6becd33b7be52c1ec26
dosecin <dosecin@qq.com>
2020-12-08 18:14:16 +0800

    感知机对偶形式

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -3,28 +3,44 @@ import random
 
 
 class Perceptron:
-    def __init__(self, n_iter: int = 1000, eta: float = 0.5):
+    def __init__(self, n_iter: int = 50, eta: float = 0.5):
         self.n_iter = n_iter
         self.eta = eta
-        self.epsilon = 0.999
 
     def fit(self, X: np.ndarray, y: np.ndarray):
-        self.w: np.ndarray = np.zeros(X.shape[1])
+        n_sample, n_feature = X.shape
+        self.w: np.ndarray = np.zeros(n_feature)
         self.b: float = 0
 
-        n_sample, n_feature = X.shape
-        n_modity = 0
         for _ in range(self.n_iter):
-            ridx = np.random.randint(0, n_sample, 1)
+            n_error = 0
+            ridx = np.random.permutation(n_sample)
             for idx in ridx:
                 xi, yi = X[idx], y[idx]
                 if yi*(np.dot(xi, self.w)+self.b) <= 0:
                     self.w += self.eta*yi*xi
                     self.b += self.eta*yi
-                    n_modity += 1
-                    if n_modity % 1000 == 0:
-                        if self.score(X, y) >= self.epsilon:
-                            break
+                    n_error += 1
+            if n_error == 0:
+                break
+
+    def fit_dual(self, X: np.ndarray, y: np.ndarray):
+        n_sample, n_feature = X.shape
+        alpha: np.ndarray = np.zeros(n_sample)
+        self.b: float = 0
+        g = np.dot(X, X.T)*y
+        for _ in range(self.n_iter):
+            n_error = 0
+            ridx = np.random.permutation(n_sample)
+            for idx in ridx:
+                yi = y[idx]
+                if yi*(np.dot(alpha, g[idx])+self.b) <= 0:
+                    alpha[idx] += self.eta
+                    self.b += self.eta*yi
+                    n_error += 1
+            if n_error == 0:
+                break
+        self.w = np.dot(alpha*y, X)
 
     def predict(self, X: np.ndarray):
         return np.sign(np.dot(X, self.w)+self.b)
--- perceptron/perceptron_test.py
+++ perceptron/perceptron_test.py
@@ -4,11 +4,25 @@ from utils import data
 from perceptron import Perceptron
 
 
-if __name__ == '__main__':
+def reduce_data(X, y):
+    w = np.where((y == 0) | (y == 1))
+    X = X[w]
+    y = y[w]
+    y[np.where(y == 0)] = -1
+    return X, y
+
+
+def load_data():
     train_X, train_y, test_X, test_y = data.read_mnist_train(os.getcwd())
+    return reduce_data(train_X, train_y), reduce_data(test_X, test_y)
+
+
+if __name__ == '__main__':
+    (train_X, train_y), (test_X, test_y) = load_data()
+
     train_y[np.where(train_y != 1)] = -1
     test_y[np.where(test_y != 1)] = -1
 
     p = Perceptron()
-    p.fit(train_X, train_y)
-    print(p.score(test_X, test_y))
+    p.fit_dual(train_X, train_y)
+    print(p.score(train_X, train_y), p.score(test_X, test_y))
